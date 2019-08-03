8ae91e41d1e0bf33b9e862a8e93afc0cccfe2bfc
Alex Mickelson <alexmickelson96@gmail.com>
2019-08-03 17:32:24 -0600

    "added comments controller"

---
+++ forumTest/Controllers/CommentsController.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using forumTest.Data;
+using forumTest.Models;
+
+namespace forumTest.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private readonly ForumsContext _context;
+
+        public CommentsController(ForumsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Comments
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.comments.ToListAsync());
+        }
+
+        // GET: Comments/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.comments
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return View(comment);
+        }
+        public async Task<List<Comment>> GetCommentsAsync(Guid pageId) {
+            return await _context.comments.Where(c => c.pageId == pageId).ToListAsync();
+        }
+
+        // POST: Comments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async void Create([Bind("Text,Id,Post,Parent")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                comment.Id = Guid.NewGuid();
+                _context.Add(comment);
+                await _context.SaveChangesAsync();
+                return ;
+            }
+            
+        }
+
+        // GET: Comments/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.comments.FindAsync(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+            return View(comment);
+        }
+
+        // POST: Comments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Text,Id,Post,Parent")] Comment comment)
+        {
+            if (id != comment.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(comment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CommentExists(comment.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(comment);
+        }
+
+        // GET: Comments/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.comments
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return View(comment);
+        }
+
+        // POST: Comments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var comment = await _context.comments.FindAsync(id);
+            _context.comments.Remove(comment);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CommentExists(Guid id)
+        {
+            return _context.comments.Any(e => e.Id == id);
+        }
+    }
+}
